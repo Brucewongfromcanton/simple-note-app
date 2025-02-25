3819bd38cb5aedc64f06d2ec96b5cf8c3a567210
Brucewongfromcanton <brucewongfromcanton@gmail.com>
2025-02-25 17:02:09 +1100

    initial version

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <textarea
+      v-model="noteContent"
+      placeholder="Start typing your notes here..."
+      @input="saveNote"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      noteContent: localStorage.getItem('noteContent') || ''
+    };
+  },
+  methods: {
+    saveNote() {
+      localStorage.setItem('noteContent', this.noteContent);
+    }
+  }
+};
+</script>
+
+<style>
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #f4f2ee;
+}
+
+#app {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+textarea {
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  resize: none;
+  padding: 20px;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+  background-color: #f4f2ee;
+  color: #333;
+  box-sizing: border-box;
+}
+</style>
